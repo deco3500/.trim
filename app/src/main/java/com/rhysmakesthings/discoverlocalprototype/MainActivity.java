a1e104f1afa2a0620e095cfc723d086257fbf238
Mark Timmins <mark.timmins@uqconnect.edu.au>
2016-09-15 22:42:54 +1000

    Condensed 2 activities that did the same thing into one activity

--- app/src/main/java/com/rhysmakesthings/discoverlocalprototype/MainActivity.java
+++ app/src/main/java/com/rhysmakesthings/discoverlocalprototype/MainActivity.java
@@ -1,11 +1,11 @@
 package com.rhysmakesthings.discoverlocalprototype;
+
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -14,15 +14,14 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
-    private GoogleMap mMap;
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private GoogleMap map;
     private GoogleApiClient mGoogleApiClient;
-    private boolean mapReady=false;
+    private boolean mapReady = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,13 +34,15 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     .build();
         }
     }
+
     @Override
     protected void onStart() {
-        mGoogleApiClient.connect();
         super.onStart();
+        mGoogleApiClient.connect();
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
+
     @Override
     protected void onStop() {
         mGoogleApiClient.disconnect();
@@ -50,27 +51,42 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+        // Assign the instance of the map to a local variable
+        map = googleMap;
+
+        // Check if location permissions are active, and if so, enable "My Location"
         if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
                 == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
+            map.setMyLocationEnabled(true);
         }
-        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+
+        // Otherwise display "No Permissions" layout
+        else {
+            setContentView(R.layout.no_permission);
+        }
+
+        // Changed temporarily for debugging.
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+
         mapReady = true;
     }
-    public void onConnected(Bundle b){
-        if (mapReady){
-            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                        mGoogleApiClient);
-                LatLng location = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-                mMap.addMarker(new MarkerOptions().position(location).title("Marker at your location"));
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+
+    public void onConnected(Bundle b) {
+        if (mapReady && ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+
+            Location lastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+            if (lastLocation != null) {
+                LatLng location = new LatLng(lastLocation.getLatitude(), lastLocation.getLongitude());
+                map.addMarker(new MarkerOptions().position(location).title("Marker at your location"));
+                map.moveCamera(CameraUpdateFactory.newLatLng(location));
             }
         }
     }
-    public void onConnectionSuspended(int a){
+
+    public void onConnectionSuspended(int a) {
 
     }
 
